553e2fef02f8196ee2f069559b1de446fdd01e73
Sylvie-lezgoooo <135068624+Sylvie-lezgoooo@users.noreply.github.com>
2024-02-19 16:45:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function gotResult(error, results)
         console.error(error);
     } else {
         console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        document.getElementById("result_object_name").innerHTML = "Object :"+results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = "Accuracy :" +results[0].confidence.toFixed(3);
     }
 }
